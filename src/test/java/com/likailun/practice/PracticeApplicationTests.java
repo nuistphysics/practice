93951cda1be0e6f552c43fdffd45f0fa4f67e990
likailun <15850732365@qq.com>
2020-10-19 16:58:58 +0800

    提交项目

---
+++ src/test/java/com/likailun/practice/PracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likailun.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
